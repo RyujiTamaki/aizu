556e1e9cc8ec664d663564e4ff440c27378eebcd
RyujiTamaki <e44119rt@gmail.com>
2020-04-22 10:58:52 +0900

    ALDS1_1_A python

---
+++ alds/1_1_A.py
@@ -0,0 +1,24 @@
+def trace(A):
+    a = " ".join([str(i) for i in A])
+    print(a)
+    return None
+
+
+def insertion_sort(A):
+    S = A.copy()
+    for i in range(1, len(A)):
+        v = S[i]
+        j = i - 1
+        while j >= 0 and S[j] > v:
+            S[j + 1] = S[j]
+            j -= 1
+        S[j + 1] = v
+        trace(S)
+    return None
+
+
+N = input()
+A = list(map(int, input().split()))
+
+trace(A)
+insertion_sort(A)
